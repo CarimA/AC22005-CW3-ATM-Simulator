45b6e80447e077b75a086d64118b27c59e5a697a
Carim A <CarimAkeju@computing.dundee.ac.uk>
2016-03-16 16:10:39 +0000

    Form Design #1
    Form design for keypad and screen.

---
+++ ATM Simulator/ATM Simulator/Forms/Master.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATM_Simulator.Forms
+{
+    public partial class Master : Form
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private void Master_Load(object sender, EventArgs e)
+        {
+            Screen sc = new Screen();
+            sc.MdiParent = this;
+            sc.Show();
+
+            PinEntry pe = new PinEntry();
+            pe.MdiParent = this;
+            pe.Show();
+        }
+    }
+}
